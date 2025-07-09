234f8a73ae0bc4e3fd9d81fc0457f2553770105b
hadiaanvd <hadia.naveed@infaque.com>
2025-07-09 03:32:42 +0200

    Updates for hosting

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ app.use(
 	cors({
 		origin: ["http://localhost:5173", "https://chemnitz-atlas.vercel.app"],
 		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
 	})
 );
 app.use(express.json());
